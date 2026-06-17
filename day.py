dde638e7b3153ef8c52756b10f82e4f663bc78d5
paras01-patel <paraschandrawanshi649@gmail.com>
2026-06-17 16:47:24 +0530

    bubbelshort

--- day.py
+++ day.py
@@ -1,14 +1,40 @@
-a=[1,2,3,4,5]
+# a = [10, 22, 34, 33]
+
+# for i in range(len(a)):  # index of array
+#     print(i)
+
+
+# a=[10,22,34,33]
+# for in range(len(a)):  #value  of array
+# print (a[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a=[5,4,3,2,1]
 n=len(a)
 c=0
 for i in range(n):
+    f=False
     for j in range(n-1):
         c=c+1
         if a[j]>a[j+1]:
             t=a[j]
             a[j]=a[j+1]
             a[j+1]=t
-print("output  ")
+            f=True
+    if f==False:
+        break
 for i in a:
     print(i,end=" ")
     print("total",c)
